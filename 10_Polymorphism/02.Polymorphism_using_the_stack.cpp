675c873927afcbe10ed950317e75ad9e8aa4fb20
seddon-software <seddon_software@btinternet.com>
2021-12-05 20:27:38 +0000

    added V-Table diagram to Polymorphism demos

--- 10_Polymorphism/02.Polymorphism_using_the_stack.cpp
+++ 10_Polymorphism/02.Polymorphism_using_the_stack.cpp
@@ -60,6 +60,43 @@ void HideControl(Control* p)
 {
     p->Hide();
 }
+/*
+    If a method is marked as virtual in the base class, the compiler creates
+    one V-Table per class and places a pointer to the V-Table in every object:
+
+          s1                   ScrollBar V-Table
+     ┌──────────┐      ┌───────────────────────────────┐
+     │  _vptr   │----->│ ScrollBar::show() {...}       │
+     │   ...    │  │   │ ScrollBar::hide() {...}       │
+     └──────────┘  │   │ ScrollBar::draw() {...}       │
+          s2       │   │ ScrollBar::~ScrollBar() {...} │
+     ┌──────────┐  │   └───────────────────────────────┘
+     │  _vptr   │--┘
+     │   ...    │ 
+     └──────────┘
+
+          b1                   Button V-Table
+     ┌──────────┐      ┌───────────────────────────────┐
+     │  _vptr   │----->│ Button::show() {...}          │
+     │   ...    │  │   │ Button::hide() {...}          │
+     └──────────┘  │   │ Button::draw() {...}          │
+          b2       │   │ Button::~Button() {...}       │
+     ┌──────────┐  │   └───────────────────────────────┘
+     │  _vptr   │--┘
+     │   ...    │ 
+     └──────────┘
+
+          t1                   TextBox V-Table
+     ┌──────────┐      ┌───────────────────────────────┐
+     │  _vptr   │----->│ TextBox::show() {...}         │
+     │   ...    │  │   │ TextBox::hide() {...}         │
+     └──────────┘  │   │ TextBox::draw() {...}         │
+          t2       │   │ TextBox::~TextBox() {...}     │
+     ┌──────────┐  │   └───────────────────────────────┘
+     │  _vptr   │--┘
+     │   ...    │ 
+     └──────────┘
+*/
 
 int main()
 {
