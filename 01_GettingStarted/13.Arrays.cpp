076743970827adf8f08444e07030cf359ea23cc7
seddon-software <seddon_software@btinternet.com>
2022-05-20 21:19:50 +0100

    reorganised fundamentals chapters

---
+++ 01_GettingStarted/13.Arrays.cpp
@@ -0,0 +1,20 @@
+/*
+ *  C++ uses the vector class to replace C's array type.  However, because the vector is dynamic, it is not
+ *  as efficient as a fixed size array.  C++11 introduced a fixed size template array class to remidy this as
+ *  a drop in replacement for vectors.
+ */
+
+#include <iostream>
+#include <array>
+using namespace std;
+
+int main()
+{
+    array v = {2, 4, 6, 8, 10};   // fixed size
+    
+    for(auto i : v)
+    {
+	    cout << i << ", " << flush;
+    }
+    cout << endl;
+}
---
+++ 03_Strings/04.Gcc_Type_Info.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <functional>
+#include <typeinfo>
+#include <cxxabi.h>    // gcc specific header to demangle names
+
+/*  
+ *  Here's a typical example of using the char* type.
+ *   
+ *  C++11 introduced many new featues to the language including type inference.  It is no
+ *  longer necessary to explicitly declare variable if the compiler already knows about the type.
+ *  You can use "auto" and "declspec" to get the compiler to work out your types.
+ * 
+ *  Unfortunately its difficult to see the types chosen by the compiler, because the compiler 
+ *  mangles symbols.  However since we are exclusively using g++ we can make use of the specific
+ *  gcc routine to demangle names shown below.  This will not work with other compilers.
+ */
+
+using namespace std;
+
+
+// this routine only works with gcc.  Note the extra include file <cxxabi.h> and changes to the Makefile.
+const char* demangle(auto p)
+{
+	int status;  // did the demangle work?
+	const char* realname = abi::__cxa_demangle(typeid(p).name(),0,0,&status);  // gcc specific code (see Makefile)
+	return realname;
+}
+
+int f(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+	// complicated function pointer
+	auto fp = &f;			// int (*fp)(int, int) = &f;
+
+    // use typeid to print the type of fp (unfortunately it's mangled by the compiler)
+    cout << "type of fp: " << typeid(fp).name() << endl;
+	
+    // use the demangle function to print type of fp 
+	cout << "type of fp: " << demangle(fp) << endl;
+}
