ebaa9d036658f7ad0b90fbdcb61a811a820aed16
seddon-software <seddon_software@btinternet.com>
2022-06-17 15:42:50 +0100

    added docs

--- 01_GettingStarted/03.Swap_using_References.cpp
+++ 01_GettingStarted/03.Swap_using_References.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 
 /*
- *
- *
+ *  In C all arguments to functions are passed by copy.  This makes it difficult to swap two vaiables (its easy to swap copies).  To overcome this limitation, C uses pointers.
+ *  C++ introduces the idea of references.  Because references act as aliases they can be used to effectively extend the scope of a variable.  In the declaration of Swap:
+ *              void Swap(int& a, int& b)
+ * 
+ *  a and b are references to x and y defined in the main program (as passed by position).  So although x and y are not in scope in Swap(), their aliases a and b are in scope.
+ * 
+ *  Notice you can use C's pointer version of Swap and C++ reference version of Swap in the same program, because the compiler can easily differentiate the two function on account of
+ *  the different parameters passed in the calls:
+ *                  Swap(&x, &y);            // pass (int*, int*)
+ *                  Swap(x, y);              // pass (int&, int&)
  */
 
 
@@ -16,12 +24,12 @@ void Swap(int* px, int* py)
     *py = temp;
 }
 
-void Swap(int& x, int& y)
+void Swap(int& a, int& b)
 {
     int temp;
-    temp = x;
-    x = y;
-    y = temp;
+    temp = a;
+    a = b;
+    b = temp;
 }
 
 
--- 01_GettingStarted/04.References_as_Aliases.cpp
+++ 01_GettingStarted/04.References_as_Aliases.cpp
@@ -1,7 +1,6 @@
 
 /*
- *
- *
+ *  References are usually used to extend the scope of a variable in another function.  However, since they are aliases they can also be used to simplify notation (as in this example).
  */
 
 #include <iostream>
@@ -14,11 +13,13 @@ int main()
 {
     vector<vector<int>> myTwoDVector = {{10, 11, 12}, {20, 21, 22}, {30, 31, 32}};
 
-    auto& v12 = myTwoDVector[1][2];
-    auto& v21 = myTwoDVector[2][1];
-
     cout << myTwoDVector[1][2] * myTwoDVector[2][1] + 
             myTwoDVector[1][2] * myTwoDVector[1][2] + 
             myTwoDVector[2][1] * myTwoDVector[2][1] << endl; 
+
+    // simplified notation using references
+    auto& v12 = myTwoDVector[1][2];
+    auto& v21 = myTwoDVector[2][1];
+
     cout << v12 * v21 + v12 * v12 + v21 * v21 << endl; 
 }
