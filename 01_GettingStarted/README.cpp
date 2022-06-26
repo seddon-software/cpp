4dd7a853bc6c9418e36076699215f3a52494884c
seddon-software <seddon_software@btinternet.com>
2022-06-26 15:04:57 +0100

    added intro REAME

---
+++ 01_GettingStarted/README.cpp
@@ -0,0 +1,23 @@
+/*
+ *  C++ is now over 40 years old.  Originally C++ was an offshoot of the C programming language with classes added.  In time C++ diverged markedly from C.
+ *  THe biggest change came in 2011 (C++11) when a whole host of features were added to the language.  Another very significant change was in 2020 (C++20).
+ * 
+ *  Now, when learning C++ you may need to:
+ * 
+ *  1) Study legacy C++; the language as it was before 2011.  This is because many systems were written in C in the 1990s and 2000s and need updating.  This means you need to be
+ *     familiar with the inner workings of C++, particularly memory mangement, its relationship to C and how strings work.
+ * 
+ *  2) Study the STL: C++11 made the language much easier to use; the main improvement was the use of templates in the Standard Template library (STL).
+ * 
+ *  3) Study templates: C++20 has continued to improve the language with great emphasis on the use of templates.  This use of templates makes C++ differ markedly from all
+ *     other modern programming languages.
+ * 
+ *  If we were just interested in writing new systems, we undoubtably should concentate on the features of C++20.  Unfortunately, there are too many legacy systems around to 
+ *  allow us to ignore C++11 and what came before.  This is what makes learning C++ so difficult: the sheer quantity of constructs in the language.  In these notes I've split
+ *  the large number of chapters into two groups.  The basic chapters with simple numbers and addvanced chapters where I've prependended the letter 'A' to their titles.
+ */
+
+
+int main()
+{
+}
