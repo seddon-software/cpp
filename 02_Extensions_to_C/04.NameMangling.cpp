9cac3864306c73e6bf8dc1c9c5b1bdc4c4ba2b6f
seddon-software <seddon_software@btinternet.com>
2023-03-14 13:28:30 +0000

    minor changes

--- 02_Extensions_to_C/04.NameMangling.cpp
+++ 02_Extensions_to_C/04.NameMangling.cpp
@@ -27,7 +27,7 @@ void ffffff(int x, double y) {}
 int main()
 {
     // inspect file with hexdump
-    string cmd = "nm 4.NameMangling.exe | grep ffffff"s; 
+    string cmd = "nm 04.NameMangling.exe | grep ffffff"s; 
     system(cmd.c_str());
 }
 
