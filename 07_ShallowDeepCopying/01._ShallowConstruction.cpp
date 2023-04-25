65f6441ab66c7915ed635f381f03968bf9c60e82
seddon-software <seddon_software@btinternet.com>
2023-04-25 22:49:18 +0100

    more docs

--- 07_ShallowDeepCopying/01._ShallowConstruction.cpp
+++ 07_ShallowDeepCopying/01._ShallowConstruction.cpp
@@ -1,3 +1,22 @@
+/*
+ *  In this example we write our own string class to illustrate potential problems with copying these objects.  
+ *  This class contains subtle bugs.  Of course, in practice we would use the string class in  the standard 
+ *  library rather than this class.
+ *  
+ *  Objects of this string class are of variable size.  Notice that they have a single attribute:
+ *              private:
+ *                  char* text;
+ *  In the const char* constructor this pointer is set to point at the heap where the character data
+ *  is stored:
+ *              text = new char [strlen(theString) + 1];
+ *              strcpy(text, theString);
+ * 
+ *  Note the +1 to allow for a null terminator when allocating space on the heap for the character array.
+ *  TO BE COMPLETED
+
+ * 
+ *  In the main   
+ */
 ////////////////////////////////////////////////////////////
 //
 //      Problems with Shallow Copy
